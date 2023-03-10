d7e42d56f4bd1509f3c9297f81355b1847da543e
IvanProkhorets <prohorets.ivan@gmail.com>
2023-03-10 02:40:26 +0200

    add API

--- src/api/searchApi.js
+++ src/api/searchApi.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
 export async function searchImage(query, currentPage) {
-  const API_KEY = '29269243-d9d53679d5364662a1466d514';
-  const BASE_URL = 'https://pixabay.com/api/';
+  const API_KEY = '9beeda1ea4aae4d04f0fe1c9b0b0fd1d';
+  const BASE_URL = 'https://api.themoviedb.org/';
 
   const response = await axios.get(`${BASE_URL}`, {
     params: {
       key: `${API_KEY}`,
-      q: `${query}`,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: 'true',
-      page: `${currentPage}`,
-      per_page: 12,
+      // q: `${query}`,
+      // image_type: 'photo',
+      // orientation: 'horizontal',
+      // safesearch: 'true',
+      // page: `${currentPage}`,
+      // per_page: 12,
     },
   });
 
