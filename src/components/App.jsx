0e0bb36828220e303cb1d52e4446936715f92262
IvanProkhorets <prohorets.ivan@gmail.com>
2023-03-18 23:50:42 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,19 @@
+// import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { ProgressBar } from 'react-loader-spinner';
 
 export const App = () => {
   return (
     <>
+      <ProgressBar />
       <Routes>
         <Route>Film</Route>
       </Routes>
+
+      <ToastContainer autoClose={3000} />
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { App } from 'components/App';
-import './index.css';
 import { theme } from './theme';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
