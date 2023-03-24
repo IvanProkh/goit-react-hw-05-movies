b239051d39838865cd52e8eb6128e3ad84ac2b0e
IvanProkhorets <prohorets.ivan@gmail.com>
2023-03-24 22:26:50 +0200

    update API

--- src/components/services/searchApi.js
+++ src/components/services/searchApi.js
@@ -1,25 +1,34 @@
 import axios from 'axios';
 
-const KEY = '9beeda1ea4aae4d04f0fe1c9b0b0fd1d';
-console.log('KEY:', KEY);
+// const KEY = '9beeda1ea4aae4d04f0fe1c9b0b0fd1d';
 
-axios.defaults.baseURL = `https://api.themoviedb.org/3`;
+// axios.defaults.baseURL = `https://api.themoviedb.org/3`;
 
-// export async function searchImage(query, currentPage) {
-//   const API_KEY = '9beeda1ea4aae4d04f0fe1c9b0b0fd1d';
-//   const BASE_URL = 'https://api.themoviedb.org/';
+const API_KEY = '9beeda1ea4aae4d04f0fe1c9b0b0fd1d';
+const BASE_URL = 'https://api.themoviedb.org/3';
 
-//   const response = await axios.get(`${BASE_URL}`, {
-//     params: {
-//       key: `${API_KEY}`,
-//       // q: `${query}`,
-//       // image_type: 'photo',
-//       // orientation: 'horizontal',
-//       // safesearch: 'true',
-//       // page: `${currentPage}`,
-//       // per_page: 12,
-//     },
-//   });
+export async function searchMovie(query, currentPage) {
+  const response = await axios.get(`${BASE_URL}/search/movie`, {
+    params: {
+      api_key: `${API_KEY}`,
+      language: 'en-US',
+      query: `${query}`,
+      include_adult: 'false',
+      page: `${currentPage}`,
+      // per_page: 12,
+    },
+  });
 
-//   return response.data;
-// }
+  return response.data;
+}
+
+export async function topMovie() {
+  const response = await axios.get(`${BASE_URL}/trending/movie/day`, {
+    params: {
+      api_key: `${API_KEY}`,
+      page: 1,
+    },
+  });
+  console.log(response);
+  return response.data;
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,3 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import { searchMovie, topMovie } from '../../components/services/searchApi';
+
+import { Box } from '../../components/Box';
 export const Home = () => {
-  return <p>ФИЛЬМЫ</p>;
+  // const [film, setFilm] = useState([]);
+
+  topMovie().then(response => {
+    console.log(response);
+    setFilm(response);
+    console.log('film:', film);
+  });
+
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      justify-content="center"
+      m={2}
+      p={3}
+    >
+      <ul>
+        {film.map(({ id, poster_path }) => (
+          <li key={id}>{poster_path}</li>
+        ))}
+      </ul>
+    </Box>
+  );
 };
