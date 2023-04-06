abfce458a172c614d0c8f45d0162d2933ff7d5f7
IvanProkhorets <prohorets.ivan@gmail.com>
2023-04-06 15:09:15 +0300

    update logic

--- src/components/Cast/Cast.js
+++ src/components/Cast/Cast.js
@@ -1,37 +1,46 @@
 // import { searchCastImage } from '../Cast/Cast';
 
 import { useEffect, useState } from 'react';
-
 import { useParams } from 'react-router-dom';
+
+import { ThreeCircles } from 'react-loader-spinner';
+import { ThreeCirclesStyle } from '../../components/services/spinner';
+import { DelayedRender } from '../services/DelayedRender';
 
 import { getMovieCast, searchImages } from '../services/searchApi';
 import { CastList, EmptyCast } from './Cast.styled';
 
 export const Cast = () => {
   const [cast, setCast] = useState([]);
+  const [loading, setLoading] = useState(false);
+  // const [showEmptyCast, setShowEmptyCast] = useState(false);
   const { movieId } = useParams();
 
   useEffect(() => {
-    getMovieCast(movieId)
-      .then(res => {
+    let timeoutId;
+    setLoading(true);
+
+    async function fetchData() {
+      try {
+        const res = await getMovieCast(movieId);
         setCast(res.cast);
-      })
-      .catch(error => {
+      } catch (error) {
         console.log(error);
-      });
+      }
+      timeoutId = setTimeout(() => setLoading(false), 300);
+    }
 
-    console.log(cast);
-    console.log(movieId);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    fetchData();
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
   }, [movieId]);
-
-  if (!cast) {
-    return;
-  }
 
   return (
     <>
-      {cast.length !== 0 ? (
+      {loading && <ThreeCircles {...ThreeCirclesStyle} />}
+      {cast && cast.length > 0 ? (
         <CastList>
           {cast.map(({ cast_id, character, name, profile_path }) => {
             const profileImage = searchImages(profile_path);
@@ -45,11 +54,81 @@ export const Cast = () => {
           })}
         </CastList>
       ) : (
-        <EmptyCast>
-          ❗❗❗Sorry, it seems that there is currently no information about the
-          actors❗❗❗
-        </EmptyCast>
+        <DelayedRender delay={300}>
+          <EmptyCast>
+            ❗❗❗Sorry, it seems that there is currently no information about
+            the actors❗❗❗
+          </EmptyCast>
+        </DelayedRender>
       )}
     </>
   );
 };
+
+// export const Cast = () => {
+//   const [cast, setCast] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const { movieId } = useParams();
+
+//   useEffect(() => {
+//     async function fetchData() {
+//       setLoading(true);
+//       try {
+//         const res = await getMovieCast(movieId);
+//         setCast(res.cast);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//       setTimeout(() => setLoading(false), 300);
+//     }
+
+//     fetchData();
+
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [movieId]);
+
+// useEffect(() => {
+//   getMovieCast(movieId)
+//     .then(res => {
+//       setCast(res.cast);
+//       setLoading(true);
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     })
+//     .finally(setLoading(false));
+
+//   console.log(cast);
+//   console.log(movieId);
+//   // eslint-disable-next-line react-hooks/exhaustive-deps
+// }, [movieId]);
+
+// if (!cast) {
+//   return;
+// }
+
+//   return (
+//     <>
+//       {loading && <ThreeCircles {...ThreeCirclesStyle} />}
+//       {cast && cast.length > 0 ? (
+//         <CastList>
+//           {cast.map(({ cast_id, character, name, profile_path }) => {
+//             const profileImage = searchImages(profile_path);
+//             return (
+//               <div key={cast_id}>
+//                 <img src={profileImage} alt={name} />
+//                 <p>{name}</p>
+//                 <p>Character: {character}</p>
+//               </div>
+//             );
+//           })}
+//         </CastList>
+//       ) : (
+//         <EmptyCast>
+//           ❗❗❗Sorry, it seems that there is currently no information about the
+//           actors❗❗❗
+//         </EmptyCast>
+//       )}
+//     </>
+//   );
+// };
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,24 +1,44 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { ThreeCircles } from 'react-loader-spinner';
+import { ThreeCirclesStyle } from '../../components/services/spinner';
 
 import { getMovieReviews } from '../services/searchApi';
+import { DelayedRender } from '../services/DelayedRender';
 
 import { ReviewsList, EmptyReviews } from './Reviews.styled';
 
 export const Reviews = () => {
   const [reviews, setReviews] = useState([]);
-
+  const [loading, setLoading] = useState(false);
   const { movieId } = useParams();
 
   useEffect(() => {
-    getMovieReviews(movieId)
-      .then(res => setReviews(res.results))
-      .catch(error => console.log(error));
+    let timeoutId;
+    setLoading(true);
+
+    async function fetchData() {
+      try {
+        const res = await getMovieReviews(movieId);
+        setReviews(res.results);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setTimeout(() => setLoading(false), 300);
+      }
+    }
+
+    fetchData();
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
   }, [movieId]);
 
   return (
     <>
-      {reviews.length !== 0 ? (
+      {loading && <ThreeCircles {...ThreeCirclesStyle} />}
+      {reviews && reviews.length > 0 ? (
         <ReviewsList>
           {reviews.map(({ id, author, content }) => {
             return (
@@ -32,10 +52,49 @@ export const Reviews = () => {
           })}
         </ReviewsList>
       ) : (
-        <EmptyReviews>
-          ❗❗❗Sorry, we don`t have any reviews for this movie❗❗❗
-        </EmptyReviews>
+        <DelayedRender delay={300}>
+          <EmptyReviews>
+            ❗❗❗Sorry, we don't have any reviews for this movie❗❗❗
+          </EmptyReviews>
+        </DelayedRender>
       )}
     </>
   );
 };
+
+// export const Reviews = () => {
+//   const [reviews, setReviews] = useState([]);
+
+//   const { movieId } = useParams();
+
+//   useEffect(() => {
+//     getMovieReviews(movieId)
+//       .then(res => setReviews(res.results))
+//       .catch(error => console.log(error));
+//   }, [movieId]);
+
+//   return (
+//     <>
+//       {reviews && reviews.length > 0 ? (
+//         <ReviewsList>
+//           {reviews.map(({ id, author, content }) => {
+//             return (
+//               <div key={id}>
+//                 <h3>
+//                   Author: <span>{author}</span>
+//                 </h3>
+//                 <p>{content}</p>
+//               </div>
+//             );
+//           })}
+//         </ReviewsList>
+//       ) : (
+//         <DelayedRender delay={300}>
+//           <EmptyReviews>
+//             ❗❗❗Sorry, we don't have any reviews for this movie❗❗❗
+//           </EmptyReviews>
+//         </DelayedRender>
+//       )}
+//     </>
+//   );
+// };
